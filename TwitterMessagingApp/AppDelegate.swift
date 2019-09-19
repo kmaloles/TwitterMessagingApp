a9c3d0ebb52a22eac33fc321771fddfb850eff47
KM <kmaloles@yondu.com>
2019-09-19 17:17:24 +0800

    twitter auth

--- TwitterMessagingApp/AppDelegate.swift
+++ TwitterMessagingApp/AppDelegate.swift
@@ -8,21 +8,33 @@
 
 import UIKit
 import CoreData
+import TwitterKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
+    private enum Constants: String {
+        case key = "HMZrDiVDPuaXIDE1XanlrD7Nj"
+        case secret = "WoNNxV9FyhxLrQFsnAzbD6FmHnPSXAglVKF0NYZS5zQoFFcLBg"
+    }
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        TWTRTwitter.sharedInstance().start(withConsumerKey: Constants.key.rawValue, consumerSecret: Constants.secret.rawValue)
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        TWTRTwitter.sharedInstance().application(app, open: url, options: options)
+        return true
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
--- TwitterMessagingApp/ViewController.swift
+++ TwitterMessagingApp/ViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
+import TwitterKit
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let logInButton = TWTRLogInButton(logInCompletion: { session, error in
+            if (session != nil) {
+                print("signed in as \(session?.userName)");
+            } else {
+                print("error: \(error?.localizedDescription)");
+            }
+        })
+        logInButton.center = self.view.center
+        self.view.addSubview(logInButton)
     }
-
+    
+    
+    @IBAction func onAuthenticateTapped(_ sender: Any) {
+        
+    }
+    
 
 }
 
