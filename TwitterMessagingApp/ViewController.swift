621865ce30bb6c4a7b767b4ce6bb45de76e04dca
KM <kmaloles@yondu.com>
2019-09-19 15:21:22 +0800

    inital commit

---
+++ TwitterMessagingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterMessagingApp
+//
+//  Created by Kevin Maloles on 19/09/2019.
+//  Copyright © 2019 Kevin Maloles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
